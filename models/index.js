0d8f278bbfa488403fe92baca94adc29cc6d6c4c
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-05-04 10:23:16 -0500

    add model associations with index.js

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require('./User');
+const Article = require('./Article');
+const Comment = require('./Comment');
+
+User.hasMany(Article, {
+  foreignKey: 'author_id',
+  onDelete: 'CASCADE'
+});
+
+Article.belongsTo(User, {
+  foreignKey: 'author_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'commenter_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'commenter_id'
+});
+
+Article.hasMany(Comment, {
+    foreignKey: post_id,
+    onDelete: 'Cascade'
+});
+
+Comment.belongsTo(Article, {
+    foreignKey: post_id,
+})
+
+
+module.exports = { User, Article , Comment};
