f58483adfd4020679839b9bd472d1e3bb69198e2
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-05-05 13:34:44 -0500

    add create method on post for comment route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -11,13 +11,13 @@ router.get('/', async (req, res) => {
     }
 });
 
-// router.post('/', async (req, res) => {
-//   try {
-//     const articleData = await Article.create(req.body);
-//     res.status(200).json(articleData);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+router.post('/', async (req, res) => {
+  try {
+    const articleData = await Comment.create(req.body);
+    res.status(200).json(articleData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
