a391a56346ee70e8a83c0a37e4793db61fea46d8
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-05-07 12:44:26 -0500

    add handlebars requirements to server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+
 // const { Article, Comment, User } = require('./models');
 
 const sequelize = require('./config/connection');
@@ -8,6 +12,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
@@ -21,9 +28,9 @@ const sess = {
 
 app.use(session(sess));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-// app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
