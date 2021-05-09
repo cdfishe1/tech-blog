9a9def7dc43a3ea9ea775931b44bb3cab8fa9eed
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-05-09 06:18:32 -0500

    create article creation method and route

--- controllers/api/article-routes.js
+++ controllers/api/article-routes.js
@@ -17,7 +17,11 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const articleData = await Article.create(req.body);
+    const articleData = await Article.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user_name,
+    });
     res.status(200).json(articleData);
   } catch (err) {
     res.status(400).json(err);
