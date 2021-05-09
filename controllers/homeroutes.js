a356d0541c37a948e4a1f6d1b12180b67604c4aa
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-05-09 12:26:46 -0500

    restyle heading

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -35,7 +35,8 @@ router.get('/articles/:id', async (req, res) => {
       include: [
         {
           model: Comment,
-          attributes: ['comment'],
+          
+          
         },
       ],
     });
