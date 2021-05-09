eef51c79b59f554a1d32ed5ed6a75feae4db01b7
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-05-09 05:21:27 -0500

    Add signup route with user creation

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -102,3 +102,15 @@ router.get('/dashboard', (req, res) => {
   });
 
 module.exports = router;
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
